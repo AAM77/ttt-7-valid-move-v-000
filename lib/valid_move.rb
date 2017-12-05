507ca9efaf42dcfc396737dd10eb2b7f42247257
Mohammad Adeel <adeel.a.mhd@gmail.com>
2017-12-05 21:26:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,7 @@
 def valid_move?(board, index)
 
   if ((0 <= index) && (index <= 8))
-    if position_taken(board, index) == true
+    if position_taken?(board, index) == true
       return true
     end
 
