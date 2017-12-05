7cc8eec1bc1aec0dc1626cca13163f608fb5d63b
Mohammad Adeel <adeel.a.mhd@gmail.com>
2017-12-05 21:12:43 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
+#Make sure that the position enter is valid (index is between 0 and 8, inclusive)
+#1. Return true if the position is between 0 and 8, inclusive.
+#2. Return false if the position is less than 0 or greater than 8
 
+def valid_move?(board, index)
+
+  if ((0 <= index) && (index <= 8))
+    return true
+    
+  else
+    return false
+  end
+
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
